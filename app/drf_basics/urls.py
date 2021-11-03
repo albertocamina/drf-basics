19e666941230e38d13672bc1cc8fa67095733c53
albertocamina <alberto.camina@sngular.com>
2021-11-03 10:45:41 +0100

    OpenAPI Schema

--- app/drf_basics/urls.py
+++ app/drf_basics/urls.py
@@ -17,7 +17,34 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 
+from django.views.generic import TemplateView
+from rest_framework.schemas import get_schema_view
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+
 urlpatterns = [
+
+    path('admin/', admin.site.urls),
+
+    
     path( 'recetario/', include("recetas.urls") ),
     path( 'auth/', include("auth_methods.urls") ),
-]
+
+    # URLS para OAuth
+    path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+
+    # Generador de Esquemas de API
+    path('openapi', get_schema_view(
+        title="Django Rest Framework Bascis",
+        description="API for testing basics elementos from Django Rest Framework",
+        version="1.0.0"
+    ), name='openapi-schema'),
+
+   # Template view para poder cargar la vista en swagger 
+    path('swagger-ui/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url':'openapi-schema'}
+    ), name='swagger-ui'),
+] + static( settings.MEDIA_URL, document_root=settings.MEDIA_ROOT )
